d38b341443e905ce5b19fbcf1b440fbd1047ba2a
Akhil <122203952+akhilupadhyay18@users.noreply.github.com>
2023-07-11 01:34:57 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  `mongodb+srv://mernuser:Chatapp123@cluster0.amweekk.mongodb.net/ChatApp?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.amweekk.mongodb.net/ChatApp?retryWrites=true&w=majority`,
   () => {
     console.log("connected to mongodb");
   }
